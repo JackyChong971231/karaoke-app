15a60f3e9649e623bd1c544a3e954ec39cd6900b
Jacky <chongkinye@gmail.com>
2026-01-31 18:58:08 -0500

    fixed the youtube new updates that block video downloading without a browser

--- downloader/yt_downloader.py
+++ downloader/yt_downloader.py
@@ -25,11 +25,16 @@ class YouTubeDownloader:
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': outtmpl,
-            'postprocessors': [],
-            'quiet': False,
             'noplaylist': True,
-            'nocheckcertificate': True,
             'ignoreerrors': True,
+
+            # 🔑 CRITICAL
+            'user_agent': (
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/121.0.0.0 Safari/537.36'
+            ),
+            'referer': 'https://www.youtube.com/',
         }
 
         try:
